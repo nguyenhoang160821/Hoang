cb4cbcc3ae0810c45e89000f0f39460da57b8a81
nguyenhoang160821 <54625158+nguyenhoang160821@users.noreply.github.com>
2020-10-16 17:59:33 +0700

    lab 01

---
+++ Lab01/Lab01/ProductList.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Lab01.Models;
+using System.Web.ModelBinding;
+namespace Lab01
+{
+    public partial class ProductList : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        public IQueryable<Product> GetProducts([QueryString("id")] int? categoryId)
+        {
+            var _db = new Lab01.Models.ProductContext();
+            IQueryable<Product> query = _db.Products;
+            if (categoryId.HasValue && categoryId > 0)
+            {
+                query = query.Where(p => p.CategoryID == categoryId);
+            }
+            return query;
+        }
+    }
+}
---
+++ Lab01/Lab01/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab01.Startup))]
+namespace Lab01
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
