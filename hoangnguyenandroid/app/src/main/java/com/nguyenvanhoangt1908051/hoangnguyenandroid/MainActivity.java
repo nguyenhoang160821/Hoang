d29bfb34bb88853d45ea03e738a75c38704c39cb
nguyenhoang160821 <54625158+nguyenhoang160821@users.noreply.github.com>
2021-04-13 16:49:23 +0700

    AndroidExam

---
+++ hoangnguyenandroid/app/src/main/java/com/nguyenvanhoangt1908051/hoangnguyenandroid/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nguyenvanhoangt1908051.hoangnguyenandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
